eaf2cdd8bd227e5b98d663b5a395963beed5204d
Micah Lund <micah.r.lund@gmail.com>
2020-07-22 21:00:09 -0600

    Working with router properties and packages.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import FullPost from './FullPost/FullPost';
+//import FullPost from './FullPost/FullPost';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
 import Posts from '../Blog/Posts/Posts';
+import {Route, Link} from 'react-router-dom';
 
 class Blog extends Component {
 
@@ -14,26 +15,32 @@ class Blog extends Component {
 
     render () {
 
-
         return (
             <div className="Blog">
-
             <header>
                 <nav>
                     <ul>
-                        <li><a href="/">Home</a></li>
-                        <li><a href="/new-post">New Post</a></li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to={{
+                            pathname: "/new-post",
+                            hash: "#submit",
+                            search: "?quick-submit=true"
+                        }}>New Post</Link></li>
                     </ul>
                 </nav>
             </header>
+            {/* <Route path="/" exact render={() => <h1>Home</h1>}/>
+            <Route path="/" render={() => <h1>Home 2</h1>}/> */}
+            <Route path="/" exact component={Posts} />
+            <Route path="/new-post" component={NewPost} />
 
+            {/* <section>
+            <FullPost id={this.state.selectedPostId} />
+            </section>
+            <section>
+            <NewPost />
+            </section> */}
 
-                <section>
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
             </div>
         );
     }
