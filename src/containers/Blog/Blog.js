d82083310c27080c20cc71668fa6c41492acdf22
Micah Lund <micah.r.lund@gmail.com>
2020-07-22 21:39:13 -0600

    Extracted route parameters and utilized switch in Blog.js.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import FullPost from './FullPost/FullPost';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
 import Posts from '../Blog/Posts/Posts';
-import {Route, NavLink} from 'react-router-dom';
+import {Route, NavLink, Switch} from 'react-router-dom';
 //import Post from '../../components/Post/Post';
 
 class Blog extends Component {
@@ -39,9 +39,11 @@ class Blog extends Component {
             </header>
             {/* <Route path="/" exact render={() => <h1>Home</h1>}/>
             <Route path="/" render={() => <h1>Home 2</h1>}/> */}
-            <Route path="/" exact component={Posts} />
-            <Route path="/new-post" component={NewPost} />
-            <Route path="/:id" exact component={FullPost} />
+            <Switch>
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
+                <Route path="/posts/:id" exact component={FullPost} />
+            </Switch>
 
             {/* <section>
             <FullPost id={this.state.selectedPostId} />
