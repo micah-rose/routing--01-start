03aa874ef1124655554e5b8709f1a00eec95d5e1
Micah Lund <micah.r.lund@gmail.com>
2020-07-22 21:10:24 -0600

    Swapped out Link with NavLink and styled the active route.

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import NewPost from './NewPost/NewPost';
 import './Blog.css';
 import Posts from '../Blog/Posts/Posts';
-import {Route, Link} from 'react-router-dom';
+import {Route, NavLink} from 'react-router-dom';
 
 class Blog extends Component {
 
@@ -20,12 +20,19 @@ class Blog extends Component {
             <header>
                 <nav>
                     <ul>
-                        <li><Link to="/">Home</Link></li>
-                        <li><Link to={{
+                        <li><NavLink 
+                        to="/" 
+                        exact 
+                        activeClassName="my-active"
+                        activeStyle={{
+                            color: "orange",
+                            textDecoration: "underline"
+                        }}>Home</NavLink></li>
+                        <li><NavLink to={{
                             pathname: "/new-post",
                             hash: "#submit",
                             search: "?quick-submit=true"
-                        }}>New Post</Link></li>
+                        }}>New Post</NavLink></li>
                     </ul>
                 </nav>
             </header>
